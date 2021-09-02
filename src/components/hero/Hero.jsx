1e273521f57d9df1dab7b8f8c54566d2ac05a725
Dakouri M. Kobri <maurille.kobri@gmail.com>
2021-09-02 23:52:42 +0200

    Fill up the here section with an introductory greeting

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,13 @@
 import "./hero.scss";
 
 export default function Hero() {
-  return <div className="hero" id="hero"></div>;
+  return (
+    <div className="hero" id="hero">
+      <div className="introduction">
+        <h2>Hi, I'm</h2>
+        <h1>Dakouri</h1>
+        <h2>a frontend developer.</h2>
+      </div>
+    </div>
+  );
 }
