86a24364440840c924fe22e18f7d40b76c92a530
Dakouri M. Kobri <maurille.kobri@gmail.com>
2021-09-04 02:16:05 +0200

    Add project data json file and list project on page with hover effect. Problem: need fix the selective hovering related to the isCompleted status of each project.

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
 import "./projects.scss";
+import projectData from "../../projectData.json";
 
 export default function Projects() {
-  return <div className="projects" id="projects"></div>;
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  // sample element
+  <div className={isCompleted ? null : "classname"}></div>;
+  return (
+    <div className="projects" id="projects">
+      <h2>Projects</h2>
+      <p>
+        Here are the projects I will be running during the Frontend course at
+        Novare Potential.
+      </p>
+      <div className="container">
+        {projectData.map((project) => (
+          <div className="card" key={project.id}>
+            <div className={project.isCompleted ? null : "card-overlay"}>
+              <img
+                className="card-thumbnail"
+                src={project.thumbnail}
+                alt={project.title}
+              />
+              <h3 className="project-status">COMING SOON</h3>
+            </div>
+            <h3 className="card-title">{project.title}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+// {project.isCompleted ?} "card card-overlay card-thumbnail project-status card-title": "card card-overlay:hover card-thumbnail project-status card-title"}
+// "card card-overlay card-thumbnail project-status card-title":
+// "card card-overlay:hover card-thumbnail project-status card-title"}
+// {project.isCompleted ? "card-overlay disable" : "card-overlay"}
