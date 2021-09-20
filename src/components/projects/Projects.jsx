63ab74ca7f6c6f4dc5f8a750381232fc8f4ee86b
Dakouri M. Kobri <maurille.kobri@gmail.com>
2021-09-20 05:16:29 +0200

    Restructuring for responsiveness and CSS to be done

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -18,59 +18,61 @@ export default function Projects({ isOpen, openModal }) {
   return (
     <div className="projects" id="projects">
       <h2>Projects</h2>
-      <p>
-        Here are the projects I will be running during the Frontend course at
-        Novare Potential.
-      </p>
-      <div className="container">
-        {data.map((project) => (
-          <div className="card">
-            <div
-              className={
-                project.isCompleted ? "card-overlay" : "card-overlay active"
-              }
-            >
-              <img
-                className="card-thumbnail"
-                key={project.id}
-                onClick={() => modalHandler(project.id)}
-                src={project.thumbnail}
-                alt={project.title}
-              />
-              <h3 className="project-status">COMING SOON</h3>
-            </div>
-            <h3 className="card-title">{project.title}</h3>
-          </div>
-        ))}
-        {data[id]["isCompleted"] && (
-          <Modal isOpen={isOpen}>
-            <div className="close-btn">
-              <button onClick={() => openModal(!isOpen)}>X</button>
-            </div>
-            <div className="modal-card">
-              <div className="modal-thumbnail">
-                <img src={data[id]["thumbnail"]} alt="" />
+      <div className="content-wrapper">
+        <p>
+          Here are the projects I will be running during the Frontend course at
+          Novare Potential.
+        </p>
+        <div className="container">
+          {data.map((project) => (
+            <div className="card">
+              <div
+                className={
+                  project.isCompleted ? "card-overlay" : "card-overlay active"
+                }
+              >
+                <img
+                  className="card-thumbnail"
+                  key={project.id}
+                  onClick={() => modalHandler(project.id)}
+                  src={project.thumbnail}
+                  alt={project.title}
+                />
+                <h3 className="project-status">COMING SOON</h3>
               </div>
-              <h3>{data[id]["title"]}</h3>
-              <p>{data[id]["description"]}</p>
-              <div className="skills">
-                {data[id]["technologyPills"].map((skills) => (
-                  <span key={data[id]["technologyPills"].indexOf(skills)}>
-                    {skills}
-                  </span>
-                ))}
-              </div>
-              <div className="btns">
-                <a href={data[id]["hostingLink"]}>
-                  <button className="btn-to-web">Visit website/app</button>
-                </a>
-                <a href={data[id]["gitRepositoryLink"]}>
-                  <button className="btn-to-repo"> Git repository</button>
-                </a>
-              </div>
+              <h3 className="card-title">{project.title}</h3>
             </div>
-          </Modal>
-        )}
+          ))}
+          {data[id]["isCompleted"] && (
+            <Modal isOpen={isOpen}>
+              <div className="close-btn">
+                <button onClick={() => openModal(!isOpen)}>X</button>
+              </div>
+              <div className="modal-card">
+                <div className="modal-thumbnail">
+                  <img src={data[id]["thumbnail"]} alt="" />
+                </div>
+                <h3>{data[id]["title"]}</h3>
+                <p>{data[id]["description"]}</p>
+                <div className="skills">
+                  {data[id]["technologyPills"].map((skills) => (
+                    <span key={data[id]["technologyPills"].indexOf(skills)}>
+                      {skills}
+                    </span>
+                  ))}
+                </div>
+                <div className="btns">
+                  <a href={data[id]["hostingLink"]}>
+                    <button className="btn-to-web">Visit website/app</button>
+                  </a>
+                  <a href={data[id]["gitRepositoryLink"]}>
+                    <button className="btn-to-repo"> Git repository</button>
+                  </a>
+                </div>
+              </div>
+            </Modal>
+          )}
+        </div>
       </div>
     </div>
   );
